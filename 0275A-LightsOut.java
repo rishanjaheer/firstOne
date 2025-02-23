d17b6688ac085ed803a4e28899c96762a9d1c335
Rishan Jaheer <79612793+rishanjaheer@users.noreply.github.com>
2025-02-23 01:59:03 -0500

    Create 0275A-LightsOut.java

---
+++ 0275A-LightsOut.java
@@ -0,0 +1,36 @@
+//this is incomplete and still in work
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[][] grid = {
+                {1, 1, 1},
+                {1, 1, 1},
+                {1, 1, 1}
+        };
+
+        int[][] input = new int[3][3];
+        for (int i  = 0; i < 3; i++) {
+            for (int j  = 0; j < 3; j++) {
+                input[i][j] = scan.nextInt();
+            }
+        }
+
+        for (int i  = 0; i < 3; i++) {
+            for (int j  = 0; j < 3; j++) {
+
+            }
+        }
+
+
+    }
+
+    public static int toggle(int times) {
+        int n = 0;
+        while (n < times) {
+            
+        }
+    }
+
+}
