f6bcf4d2b4a818eceec31671500e3a30b8dbd374
Rishan Jaheer <79612793+rishanjaheer@users.noreply.github.com>
2025-02-22 22:08:56 -0500

    Update 69A-YoungPhysicist.java

--- 69A-YoungPhysicist.java
+++ 69A-YoungPhysicist.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class Problem69A {
+public class main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
