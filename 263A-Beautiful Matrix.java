c06bc685ad954376fb5361b34c5a6eb6f5009f2a
Rishan Jaheer <79612793+rishanjaheer@users.noreply.github.com>
2025-02-22 22:09:24 -0500

    Update 263A-Beautiful Matrix.java

--- 263A-Beautiful Matrix.java
+++ 263A-Beautiful Matrix.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         
         Scanner s = new Scanner(System.in);
